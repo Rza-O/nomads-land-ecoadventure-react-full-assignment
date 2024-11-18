cf2a2c09b64d9a523e09d6ae06cdbb8e00f37ba2
appoclayptor <shahreza1414@gmail.com>
2024-11-18 19:54:02 +0600

    showed the card dynamically

--- src/Components/AdventureCard.jsx
+++ src/Components/AdventureCard.jsx
@@ -1,10 +1,30 @@
 
 
+// eslint-disable-next-line react/prop-types
 const AdventureCard = ({singleData}) => {
+    const { title, image, ecoFriendlyFeatures } = singleData
     console.log(singleData);
     return (
-        <div>
-            Adventure card
+        <div className="card bg-base-100 shadow-xl">
+            <figure>
+                <img
+                    src={image}
+                    className="md:h-32 lg:h-40 w-full"
+                    alt={title} />
+            </figure>
+            <div className="card-body justify-between text-pretty">
+                <h2 className="card-title">{title}</h2>
+                <div>
+                    <p className="font-semibold">Features</p>
+                    {
+                        // eslint-disable-next-line react/prop-types
+                        ecoFriendlyFeatures.map((singleFeatures, idx)=> <li className="font-light" key={idx}>{singleFeatures}</li>)
+                    }
+                </div>
+                <div className="card-actions justify-end">
+                    <button className="btn  text-white bg-optional border-none hover:bg-Tertiary">Explore Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Adventures.jsx
+++ src/Components/Adventures.jsx
@@ -10,10 +10,12 @@ const Adventures = ({data}) => {
                 <h2 className="text-4xl text-center text-Tertiary font-bold">Adventure Explorer</h2>
                 <p className="font-light">Discover a world of eco-friendly adventures tailored for every thrill-seeker. From serene mangrove kayaking to adrenaline-filled mountain treks, our experiences connect you with nature while preserving it.</p>
             </div>
-            {
-                // eslint-disable-next-line react/prop-types
-                data.map(singleData => <AdventureCard key={singleData.id} singleData={singleData}></AdventureCard>)
-            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-11/12 mx-auto gap-3 mt-8">
+                {
+                    // eslint-disable-next-line react/prop-types
+                    data.map(singleData => <AdventureCard key={singleData.id} singleData={singleData}></AdventureCard>)
+                }
+            </div>
         </div>
     );
 };
