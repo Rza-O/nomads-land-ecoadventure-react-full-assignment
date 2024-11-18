cc3c26afcb516ed91573bdd4bf739b81786a50bb
appoclayptor <shahreza1414@gmail.com>
2024-11-18 10:08:00 +0600

    Installed necessary dependencies and packages

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCD6ipBApQUKPdNDw_l5THpSnke0zs6vvY",
+    authDomain: "a09-eco-adventure-auth.firebaseapp.com",
+    projectId: "a09-eco-adventure-auth",
+    storageBucket: "a09-eco-adventure-auth.firebasestorage.app",
+    messagingSenderId: "541698401377",
+    appId: "1:541698401377:web:413d5d4bf10facaf3af620"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
