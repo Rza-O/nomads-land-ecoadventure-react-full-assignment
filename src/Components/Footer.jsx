3a75e2934c7afd21781a9ee6949fc2256de4beac
appoclayptor <shahreza1414@gmail.com>
2024-11-18 15:16:57 +0600

    added fake json data

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center bg-base-300 text-base-content rounded p-10 ">
+        <footer className="footer footer-center bg-base-300 rounded p-10 text-optional">
             <nav className="grid grid-flow-col gap-4">
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
