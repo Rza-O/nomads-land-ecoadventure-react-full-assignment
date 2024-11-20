a78c777b316227e578de956ec3f6b3c1e4946264
appoclayptor <shahreza1414@gmail.com>
2024-11-20 21:32:23 +0600

    hid all the enviromental variables

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCD6ipBApQUKPdNDw_l5THpSnke0zs6vvY",
-    authDomain: "a09-eco-adventure-auth.firebaseapp.com",
-    projectId: "a09-eco-adventure-auth",
-    storageBucket: "a09-eco-adventure-auth.firebasestorage.app",
-    messagingSenderId: "541698401377",
-    appId: "1:541698401377:web:413d5d4bf10facaf3af620"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
