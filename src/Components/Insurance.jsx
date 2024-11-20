36837b4443aba6bab254b8f1fcbabb7e10ddf271
appoclayptor <shahreza1414@gmail.com>
2024-11-20 21:56:26 +0600

    some minor responsive debugging

--- src/Components/Insurance.jsx
+++ src/Components/Insurance.jsx
@@ -10,10 +10,10 @@ const Insurance = () => {
     }, []);
     return (
         <div className='md:flex justify-evenly items-center w-11/12 mx-auto space-x-6 space-y-6 text-center md:text-left mb-3 lg:mb-0'>
-            <div data-aos="fade-right">
+            <div data-aos="zoom-in">
                 <img src={world} alt="" />
             </div>
-            <div className='lg:space-y-4 space-y-2' data-aos="fade-left">
+            <div className='lg:space-y-4 space-y-2' data-aos="zoom-in">
                 <h2 className='font-bold text-3xl'>Get an instant travel insurance quote</h2>
                 <p className='lg:w-96  font-light'>Simple and flexible travel insurance designed by travelers, for travelers. See why over 3 million travelers have chosen World Nomads for more than 20 years.</p>
                 <div className='flex justify-center md:block'>
